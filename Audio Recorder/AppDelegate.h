88645004fe53d58e02c7895c3fdf7a318e477330
Aislin Black <ablack17@newarka.edu>
2016-07-06 10:25:32 -0400

    Initial Commit

---
+++ Audio Recorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Audio Recorder
+//
+//  Created by Aislin Philomena Black on 7/6/16.
+//  Copyright © 2016 Aislin Philomena Black. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Audio Recorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Audio Recorder
+//
+//  Created by Aislin Philomena Black on 7/6/16.
+//  Copyright © 2016 Aislin Philomena Black. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
